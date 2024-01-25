73a1883a6c47a7af697429141b9fa868b3e05f01
Blurri10 <mbasilnore.10@gmail.com>
2024-01-25 13:39:29 +0530

    updated sol for 0217

--- 0217 Contains Duplicate.js
+++ 0217 Contains Duplicate.js
@@ -12,23 +12,28 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    const counts = {};
-    for (let i of nums) {
-        if (counts[i] === undefined) {
-            counts[i] = 1;
-        }
-        else {
-            counts[i]++;
-        }
-    }
+    const set = new Set(nums);
+    return set.size !== nums.length;
 
-    const testArray = Object.values(counts).sort();
-    if (testArray[testArray.length-1] >= 2) {
-        return true;
-    }
-    else return false;
+    // previous solution that worked:
+    //
+    // const counts = {};
+    // for (let i of nums) {
+    //     if (counts[i] === undefined) {
+    //         counts[i] = 1;
+    //     }
+    //     else {
+    //         counts[i]++;
+    //     }
+    // }
+    //
+    // const testArray = Object.values(counts).sort();
+    // if (testArray[testArray.length-1] >= 2) {
+    //     return true;
+    // }
+    // else return false;
 
-    // previous solution that wasn't accepted
+    // previous solution that worked but wasn't accepted
     //
     // for (let i=0; i<nums.length; i++) {
     //     if (nums.indexOf(nums[i], i+1) !== -1) {
